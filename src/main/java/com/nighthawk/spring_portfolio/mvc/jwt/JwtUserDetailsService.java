6c7915ba8638afbc8765ec5053ae27b4a158adf5
Aadit Gupta <spacegiant21@gmail.com>
2023-01-26 09:25:58 -0800

    jwt auth

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.nighthawk.spring_portfolio.mvc.jwt;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Autowired
+    private com.nighthawk.spring_portfolio.mvc.user.UserJpaRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.nighthawk.spring_portfolio.mvc.user.User user = repository.findByEmail(username);
+		if (user != null) {
+			return new User(user.getEmail(), user.getPassword(),
+					new ArrayList<>());
+		} else {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+	}
+}
