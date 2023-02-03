be9433bc6dc31e16716634ecf8acac5f21b73b41
Don Tran <donqt15@gmail.com>
2023-02-03 09:27:41 -0800

    ser api requests in own file and new db users

--- src/main/java/com/nighthawk/spring_portfolio/mvc/team/TeamApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/team/TeamApiController.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import com.nighthawk.spring_portfolio.mvc.user.User;
 import com.nighthawk.spring_portfolio.mvc.user.UserJpaRepository;
-import com.nighthawk.spring_portfolio.mvc.role.Role;
 import com.nighthawk.spring_portfolio.mvc.role.RoleJpaRepository;
 import com.nighthawk.spring_portfolio.mvc.drivelog.DriveLog;
 
@@ -18,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.text.SimpleDateFormat;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/team")
 public class TeamApiController {
     // Autowired enables Control to connect POJO Object through JPA
     @Autowired
@@ -30,22 +27,13 @@ public class TeamApiController {
     @Autowired
     private RoleJpaRepository roleRepository;
 
-    Map<String, Integer> teamToUser = new HashMap<String, Integer>();
-
     /*
      * GET List of Teams
      */
-    @GetMapping("/teamsList")
+    // TODO: 500 error and idky
+    @GetMapping("/")
     public ResponseEntity<List<Team>> getTeams() {
         return new ResponseEntity<>(teamRepository.findAllByOrderByNameAsc(), HttpStatus.OK);
-    }
-
-    /*
-     * GET List of users
-     */
-    @GetMapping("/usersList")
-    public ResponseEntity<List<User>> getUsers() {
-        return new ResponseEntity<>(userRepository.findAllByOrderByNameAsc(), HttpStatus.OK);
     }
 
     // TODO: needs security access since we only want admins to create a new team
@@ -56,59 +44,6 @@ public class TeamApiController {
         Team team = new Team(name, location);
         teamRepository.save(team);
         return new ResponseEntity<>(name + " team has been successfully created", HttpStatus.CREATED);
-    }
-
-    // creates new user
-    @PostMapping(value = "/newUser", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> newUser(@RequestParam("email") String email,
-            @RequestParam("password") String password,
-            @RequestParam("name") String name,
-            @RequestParam("dob") String dobString,
-            @RequestParam("gender") char gender,
-            @RequestParam("teamName") String teamName) {
-
-        // Create DOB
-        Date dob;
-
-        try {
-            dob = new SimpleDateFormat("MM-dd-yyyy").parse(dobString);
-        } catch (Exception e) {
-            return new ResponseEntity<>(dobString + " error; try MM-dd-yyyy",
-                    HttpStatus.BAD_REQUEST);
-        }
-
-        // find team by name
-        Team team = teamRepository.findByName(teamName);
-
-        // if team is not null then it adds user to db, if not it sends bad request
-        if (team != null) {
-            Role roleStudent = roleRepository.findByName("User");
-            password = BCrypt.hashpw(password, BCrypt.gensalt());
-            User user = new User(email, password, gender, name, dob, roleStudent, 100.0);
-
-            team.getUsers().add(user);
-            teamRepository.save(team); // conclude by writing the user updates
-
-            // return email (or return w message of successfully created user)
-            return new ResponseEntity<>(email + " user created successfully", HttpStatus.OK);
-        } else {
-            // returns team name could not be found and bad request
-            return new ResponseEntity<>("team name is invalid", HttpStatus.BAD_REQUEST);
-        }
-
-    }
-
-    @PostMapping("/updateRole")
-    public ResponseEntity<Object> updateRole(@RequestParam("email") String email,
-            @RequestParam("roleName") String roleName) {
-        User user = userRepository.findByEmail(email);
-        if (user != null) {
-            Role role = roleRepository.findByName(roleName);
-            user.getRoles().add(role);
-            userRepository.save(user);
-            return new ResponseEntity<>(email + " role updated", HttpStatus.OK);
-        }
-        return new ResponseEntity<>("user not found", HttpStatus.BAD_REQUEST);
     }
 
     @Transactional
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/user/UserApiController.java
@@ -0,0 +1,94 @@
+package com.nighthawk.spring_portfolio.mvc.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import com.nighthawk.spring_portfolio.mvc.user.User;
+import com.nighthawk.spring_portfolio.mvc.user.UserJpaRepository;
+import com.nighthawk.spring_portfolio.mvc.role.Role;
+import com.nighthawk.spring_portfolio.mvc.role.RoleJpaRepository;
+import com.nighthawk.spring_portfolio.mvc.team.Team;
+import com.nighthawk.spring_portfolio.mvc.team.TeamJpaRepository;
+import com.nighthawk.spring_portfolio.mvc.drivelog.DriveLog;
+
+import java.util.*;
+
+import org.springframework.transaction.annotation.Transactional;
+import java.text.SimpleDateFormat;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+    @Autowired
+    private TeamJpaRepository teamRepository;
+
+    @Autowired
+    private UserJpaRepository userRepository;
+
+    @Autowired
+    private RoleJpaRepository roleRepository;
+
+    /*
+     * GET List of users
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userRepository.findAllByOrderByNameAsc(), HttpStatus.OK);
+    }
+
+    // creates new user
+    @PostMapping(value = "/newUser", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> newUser(@RequestParam("email") String email,
+            @RequestParam("password") String password,
+            @RequestParam("name") String name,
+            @RequestParam("dob") String dobString,
+            @RequestParam("gender") char gender,
+            @RequestParam("teamName") String teamName) {
+
+        // Create DOB
+        Date dob;
+
+        try {
+            dob = new SimpleDateFormat("MM-dd-yyyy").parse(dobString);
+        } catch (Exception e) {
+            return new ResponseEntity<>(dobString + " error; try MM-dd-yyyy",
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // find team by name
+        Team team = teamRepository.findByName(teamName);
+
+        // if team is not null then it adds user to db, if not it sends bad request
+        if (team != null) {
+            Role roleStudent = roleRepository.findByName("User");
+            password = BCrypt.hashpw(password, BCrypt.gensalt());
+            User user = new User(email, password, gender, name, dob, roleStudent, 100.0);
+
+            team.getUsers().add(user);
+            teamRepository.save(team); // conclude by writing the user updates
+
+            // return email (or return w message of successfully created user)
+            return new ResponseEntity<>(email + " user created successfully", HttpStatus.OK);
+        } else {
+            // returns team name could not be found and bad request
+            return new ResponseEntity<>("team name is invalid", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @PostMapping("/updateRole")
+    public ResponseEntity<Object> updateRole(@RequestParam("email") String email,
+            @RequestParam("roleName") String roleName) {
+        User user = userRepository.findByEmail(email);
+        if (user != null) {
+            Role role = roleRepository.findByName(roleName);
+            user.getRoles().add(role);
+            userRepository.save(user);
+            return new ResponseEntity<>(email + " role updated", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("user not found", HttpStatus.BAD_REQUEST);
+    }
+}
