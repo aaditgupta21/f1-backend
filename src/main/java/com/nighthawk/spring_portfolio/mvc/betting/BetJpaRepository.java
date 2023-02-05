a755115234defe6eec67b11f05635835cd177ca5
Don Tran <donqt15@gmail.com>
2023-02-05 14:26:48 -0800

    bet and drive log jpa repo initialized

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/betting/BetJpaRepository.java
@@ -0,0 +1,22 @@
+package com.nighthawk.spring_portfolio.mvc.betting;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/*
+Extends the JpaRepository interface from Spring Data JPA.
+-- Java Persistent API (JPA) - Hibernate: map, store, update and retrieve database
+-- JpaRepository defines standard CRUD methods
+-- Via JPA the developer can retrieve database from relational databases to Java objects and vice versa.
+ */
+
+public interface BetJpaRepository extends JpaRepository<Bet, Long> {
+    List<Bet> findAllByOrderByIdAsc();
+
+    // Custom JPA query
+    @Query(value = "SELECT * FROM Person p WHERE p.name LIKE ?1 or p.email LIKE ?1", nativeQuery = true)
+    List<Bet> findByLikeTermNative(String term);
+}
