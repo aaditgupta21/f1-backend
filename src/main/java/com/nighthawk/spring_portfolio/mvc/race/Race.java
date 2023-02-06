b699f0d3d901f2cd6ef414e2b8262c88da5cdaea
Don Tran <donqt15@gmail.com>
2023-02-06 00:19:12 -0800

    init file that fills db w all races from 1950-now

--- src/main/java/com/nighthawk/spring_portfolio/mvc/race/Race.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/race/Race.java
@@ -12,6 +12,8 @@ import javax.validation.constraints.Positive;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.mongodb.lang.NonNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -27,6 +29,7 @@ public class Race {
     @NotEmpty
     private String circuit;
 
+    @NonNull
     @DateTimeFormat(pattern = "MM-dd-yyyy")
     private Date date;
 
@@ -36,14 +39,19 @@ public class Race {
     @NotEmpty
     private String location;
 
-    public Race(String name, String circuit, Date date, int round, String location) {
+    @NotEmpty
+    private String season;
+
+    public Race(String name, String circuit, Date date, int round, String location, String season) {
         this.name = name;
         this.circuit = circuit;
         this.date = date;
         this.round = round;
         this.location = location;
+        this.season = season;
     }
 
+    // TODO: add season
     public String toString() {
         return ("{ \"raceName\": " + this.name + ", " + "\"date\": " + this.date + ", " + "\"round\": "
                 + this.round + ", " + "\"location\": " + this.location + " }");
