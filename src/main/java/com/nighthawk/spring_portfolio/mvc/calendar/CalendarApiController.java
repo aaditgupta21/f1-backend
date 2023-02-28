580694e8a9a39363fa1c2e4b2f0a01d39d198687
icyGS <108628576+icyGS@users.noreply.github.com>
2023-02-27 21:12:39 -0800

    added update and delete routes for calendar

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarApiController.java
@@ -37,6 +37,51 @@ public class CalendarApiController {
         return new ResponseEntity<>(event + " listed successfully!", HttpStatus.CREATED);
     }
 
+
+    @PostMapping("/updateCalendar")
+    public ResponseEntity<Object> updateUser(@RequestBody final Map<String, Object> map) {
+
+        // Create date
+        String event = (String) map.get("event");
+        String dateOfEvent = (String) map.get("dateOfEvent");
+        String note = (String) map.get("note");
+        Date date;
+
+        Calendar calendar = calendarJpaRepository.findByEvent(event);
+
+        try {
+            date = new SimpleDateFormat("MM-dd-yyyy").parse(dateOfEvent);
+        } catch (Exception e) {
+            return new ResponseEntity<>(dateOfEvent + " error; try MM-dd-yyyy",
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if (calendar != null) {
+            calendar.setEvent(event);
+            calendar.setDateOfEvent(date);
+            calendar.setNote(note);
+
+
+            calendarJpaRepository.save(calendar);
+
+            return new ResponseEntity<>(event + " event updated", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("user not found", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Object> deleteUser(@RequestBody final Map<String, Object> map) {
+        String event = (String) map.get("event");
+        Calendar calendar = calendarJpaRepository.findByEvent(event);
+
+        if (calendar != null) {
+            calendarJpaRepository.delete(calendar);
+            return new ResponseEntity<>(event + " user deleted", HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("user not found", HttpStatus.BAD_REQUEST);
+    }
+
     /*
      * GET List of Races
      */
